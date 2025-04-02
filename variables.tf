80675787b2629f28ad9dc6aba2c49e8966ad8155
orumpark94 <nowd940@gmail.com>
2025-04-02 11:21:11 +0900

    ami id 값 수정

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
-# access_key와 secret_key는 GitHub Actions 환경변수로 대체되므로 변수 제거
-
 variable "region" {
   default = "ap-northeast-2"  
 }
 
 variable "ami_id" {
-  default = "ami-0e001c9271cf7f3b9"  
+  default = "ami-0fa377108253bf620"
 }
 
 variable "instance_type" {
