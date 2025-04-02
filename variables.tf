f3dce7f6806b2f06859e2e53885c810f117f0417
orumpark94 <nowd940@gmail.com>
2025-04-02 12:04:38 +0900

    network 설정 추가

--- variables.tf
+++ variables.tf
@@ -1,13 +1,25 @@
+# 기본 설정들
 variable "region" {
   default = "ap-northeast-2"
 }
 
 variable "ami_id" {
-  default = "ami-0c1989c90aa86e7cf"
+  default = "ami-0c1989c90aa86e7cf"  # Ubuntu 22.04 (x86_64)
 }
 
 variable "instance_type" {
   default = "t2.micro"
 }
 
+# 네트워크 설정 추가
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
 
+variable "subnet_cidr" {
+  default = "10.0.1.0/24"
+}
+
+variable "availability_zone" {
+  default = "ap-northeast-2a"
+}
