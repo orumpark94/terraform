0dc58382dd6431d2a1d061dfafd9305fba6b53db
orumpark94 <nowd940@gmail.com>
2025-04-07 08:39:25 +0900

    두개의 인스턴스 생성

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -149,7 +149,7 @@ resource "aws_db_instance" "mysql" {
   identifier              = "mydb"
   engine                  = "mysql"
   engine_version          = "8.0"
-  instance_class          = "db.t3.micro"
+  instance_class          = "db.t2.micro"
   allocated_storage       = 20
   username                = "admin"
   password                = var.db_password
@@ -162,3 +162,4 @@ resource "aws_db_instance" "mysql" {
     Name = "rds-mysql"
   }
 }
+
