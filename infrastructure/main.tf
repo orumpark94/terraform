0291971172398babede17934c33895088db6988f
orumpark94 <nowd940@gmail.com>
2025-04-07 09:39:49 +0900

    RDS 버전 수정

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -167,7 +167,7 @@ resource "aws_db_instance" "mysql" {
   identifier              = "mydb"
   engine                  = "mysql"
   engine_version          = "8.0.35"
-  instance_class          = "db.t2.micro"
+  instance_class          = "db.t3.micro"
   allocated_storage       = 20
   username                = "admin"
   password                = var.db_password
