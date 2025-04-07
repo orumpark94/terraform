98d910c5d931b69a38aea886e56e1577c633d85b
orumpark94 <nowd940@gmail.com>
2025-04-07 09:04:04 +0900

    outputs.tf 파일 수정

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -1,7 +1,21 @@
-output "instance_id" {
-  value = aws_instance.minimal.id
+# EC2 Web 인스턴스 출력
+output "web_instance_id" {
+  description = "Web EC2 인스턴스의 ID"
+  value       = aws_instance.web_instance.id
 }
 
-output "public_ip" {
-  value = aws_instance.minimal.public_ip
+output "web_instance_public_ip" {
+  description = "Web EC2 인스턴스의 퍼블릭 IP 주소"
+  value       = aws_instance.web_instance.public_ip
+}
+
+# RDS MySQL 인스턴스 출력
+output "rds_endpoint" {
+  description = "RDS 인스턴스의 엔드포인트 주소"
+  value       = aws_db_instance.mysql.endpoint
+}
+
+output "rds_port" {
+  description = "RDS 인스턴스의 포트"
+  value       = aws_db_instance.mysql.port
 }
