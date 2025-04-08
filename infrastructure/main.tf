4672361f368f79f262b430479daa0004ee0fb0a4
orumpark94 <nowd940@gmail.com>
2025-04-08 09:45:32 +0900

    db name 수정

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -171,7 +171,7 @@ resource "aws_db_instance" "mysql" {
   allocated_storage       = 20
   username                = "admin"
   password                = var.db_password
-  name                    = "mydatabase"
+  db_name                    = "mydatabase"
   skip_final_snapshot     = true
   db_subnet_group_name    = aws_db_subnet_group.db_subnet_group.name
   vpc_security_group_ids  = [aws_security_group.db_sg.id]
